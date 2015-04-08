e1a27b051e1a9f28fc2b8a255a3544c02dc10c54
Christiaan Conover <cconover@moneymappress.com>
2015-04-08 12:54:55 -0400

    Adds additional tests for the theme function. NOT COMPLETE.

--- tests/test-theme-function.php
+++ tests/test-theme-function.php
@@ -1,11 +1,27 @@
 <?php
 // Test the outputs of this plugin
 
+namespace cconover\FeaturedImageCaption;
+
 // Theme function
-class Theme_Function_Test extends WP_UnitTestCase {
+class Theme_Function_Test extends \WP_UnitTestCase
+{
     // No arguments, use function defaults
-    function test_no_args() {
+    public function test_no_args()
+    {
         $output = cc_featured_image_caption();
-        $this->expectOutputString( $output );
+        $this->expectOutputString($output);
+    }
+
+    // Return result
+    public function test_return()
+    {
+        $output = cc_featured_image_caption(false);
+    }
+
+    // Echo is false, HTML is true. HTML should be overridden and return array.
+    public function test_echo_html_override()
+    {
+        $output = cc_featured_image_caption(false, true);
     }
 }
