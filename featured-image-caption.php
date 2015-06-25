0b2bc50ac5d5cd504649e5c3623513e30a1e3de1
Christiaan Conover <dev@cconover.com>
2015-06-25 01:56:26 -0400

    Moves the plugin activation process from a method in the Manage class to a function in the primary plugin file.

--- featured-image-caption.php
+++ featured-image-caption.php
@@ -11,41 +11,35 @@
  */
 
 // Prevent direct access
-if ( ! defined( 'ABSPATH' ) ) {
-    die( 'You cannot access this resource directly.' );
+if (!defined('ABSPATH')) {
+    die('You cannot access this resource directly.');
 }
 
 // Check that the version of PHP is sufficient
-if( version_compare( phpversion(), '5.3', '<' ) ) {
-    trigger_error( 'PHP version ' . phpversion() . ' is unsupported, must be version 5.3 or newer', E_USER_ERROR );
+if (version_compare(phpversion(), '5.3', '<')) {
+    trigger_error('PHP version '.phpversion().' is unsupported, must be version 5.3 or newer', E_USER_ERROR);
 
     return;
 }
 
 /* Define plugin constants */
-define( 'CCFIC_ID', 'cc-featured-image-caption' ); // Plugin ID
-define( 'CCFIC_NAME', 'Featured Image Caption' ); // Plugin name
-define( 'CCFIC_VERSION', '0.8.0' ); // Plugin version
-define( 'CCFIC_WPVER', '3.5' ); // Minimum required version of WordPress
-define( 'CCFIC_KEY', 'cc_featured_image_caption' ); // Database key
+define('CCFIC_ID', 'cc-featured-image-caption'); // Plugin ID
+define('CCFIC_NAME', 'Featured Image Caption'); // Plugin name
+define('CCFIC_VERSION', '0.8.0'); // Plugin version
+define('CCFIC_WPVER', '3.5'); // Minimum required version of WordPress
+define('CCFIC_KEY', 'cc_featured_image_caption'); // Database key
+define('CCFIC_PATH', __FILE__); // Path to the primary plugin file
 
-
-// Plugin activation
-if( is_admin() ) {
-    require_once 'classes/Manage.php';
+if (is_admin()) {
     // Plugin activation
-    register_activation_hook( __FILE__, array( '\cconover\FeaturedImageCaption\Manage', 'activate' ) );
+    register_activation_hook(__FILE__, 'ccfic_activate');
 }
 
 /**
  * Plugin loader hook.
  */
-function cc_featured_image_caption_loader() {
-    // Define the path to this file
-    if ( ! defined( 'CCFIC_PATH' ) ) {
-        define( 'CCFIC_PATH', __FILE__ );
-    }
-
+function cc_featured_image_caption_loader()
+{
     // Composer autoloader
     require_once 'vendor/autoload.php';
 
@@ -66,13 +60,14 @@ add_action('plugins_loaded', 'cc_featured_image_caption_loader');
  *
  * @return string The formatted caption.
  */
-function cc_featured_image_caption( $echo = true, $html = true ) {
+function cc_featured_image_caption($echo = true, $html = true)
+{
     // Call the caption data using the shortcode
     $format = $html ? '' : ' format="plaintext"';
-    $caption = do_shortcode( '[ccfic'.$format.']');
+    $caption = do_shortcode('[ccfic'.$format.']');
 
     // If the result should be printed to the screen.
-    if ( $echo ) {
+    if ($echo) {
         echo $caption;
     } else {
         return $caption;
@@ -89,6 +84,31 @@ function cc_featured_image_caption( $echo = true, $html = true ) {
  *
  * @return bool
  */
-function cc_has_featured_image_caption() {
+function cc_has_featured_image_caption()
+{
     return true;
+}
+
+/**
+ * Plugin activation.
+ *
+ * @since 0.8.1
+ */
+function ccfic_activate()
+{
+    // Check to make sure the version of WordPress being used is compatible with the plugin
+    if (version_compare(get_bloginfo('version'), CCFIC_WPVER, '<')) {
+        wp_die('Your version of WordPress is too old to use this plugin. Please upgrade to the latest version of WordPress.');
+    }
+
+    // Default plugin options
+    $options = new \stdClass();
+    $options->version = CCFIC_VERSION; // Current plugin version
+    $options->auto_append = true; // Automatically append caption to featured image
+    $options->container = true; // Wrap the caption HTML in a container div
+
+    // Add options to database
+    $result = add_option(CCFIC_KEY.'_options', $options);
+
+    return $result;
 }
