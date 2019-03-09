6a7d15f1633561ba015065e9e27ed378badc66b3
Christiaan Conover <christiaan@cconover.com>
2019-03-09 10:40:58 -0500

    Remove Composer autoloader
    Remove the Composer autoloader and add a native one in the main plugin
    file. This means that the plugin no longer needs to deliver any Composer
    dependencies for production, making it much lighter weight.

--- featured-image-caption.php
+++ featured-image-caption.php
@@ -35,13 +35,47 @@ if ( is_admin() ) {
 }
 
 /**
+ * Class autoloader
+ *
+ * Auto-loads classes for the plugin.
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register( function ( $class ) {
+
+    // project-specific namespace prefix
+    $prefix = 'cconover\\FeaturedImageCaption\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/classes/';
+
+    // does the class use the namespace prefix?
+    $len = strlen( $prefix );
+    if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr( $class, $len );
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
+
+    // if the file exists, require it
+    if ( file_exists( $file ) ) {
+        require $file;
+    }
+} );
+
+/**
  * Plugin loader hook.
  */
 function cc_featured_image_caption_loader()
 {
-    // Composer autoloader
-    require_once __DIR__ . '/vendor/autoload.php';
-
     // Instantiate the plugin
     $bootstrap = new \cconover\FeaturedImageCaption\Bootstrap();
     $bootstrap->load();
