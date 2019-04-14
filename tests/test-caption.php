a53e290896a8819e98c7cdf7f30e37ef1a56d43f
Christiaan Conover <christiaan@cconover.com>
2019-04-14 16:21:10 -0400

    Fix test environment setup
    Run the plugin activation function when bootstrapping tests, to ensure
    that all of the default settings are in place. Fix the PHPUnit suite
    name. PHP linting fixes.

--- tests/test-caption.php
+++ tests/test-caption.php
@@ -4,7 +4,8 @@
  * Test the methods in the Caption class.
  */
 
-class CaptionTest extends WP_UnitTestCase {
+class CaptionTest extends WP_UnitTestCase
+{
     /**
      * Instance of the Caption class.
      */
@@ -21,9 +22,10 @@ class CaptionTest extends WP_UnitTestCase {
     private $string;
 
     /**
-     * Class constructor.
+     * Set up the test suite properties.
      */
-    public function __construct() {
+    private function set_test_data()
+    {
         // Instantiate the Caption class instance
         $this->caption = new \cconover\FeaturedImageCaption\Caption();
 
@@ -48,315 +50,407 @@ class CaptionTest extends WP_UnitTestCase {
     /**
      * caption() method, no arguments.
      */
-    public function test_caption() {
+    public function test_caption()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         global $post;
 
         // Create a new post
         $post = $this->factory->post->create_and_get();
-        setup_postdata( $post );
+        setup_postdata($post);
 
         // Add the caption data to the post meta
-        add_post_meta( $post->ID, '_cc_featured_image_caption', $this->postmeta );
+        add_post_meta($post->ID, '_cc_featured_image_caption', $this->postmeta);
 
         // Get the output from the method
         $test = $this->caption->caption();
 
-        $this->assertEquals( $this->string->html, $test );
+        $this->assertEquals($this->string->html, $test);
     }
 
     /**
      * caption() method, return array.
      */
-    public function test_caption_array() {
+    public function test_caption_array()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         global $post;
 
         // Create a new post
         $post = $this->factory->post->create_and_get();
-        setup_postdata( $post );
+        setup_postdata($post);
 
         // Add the caption data to the post meta
-        add_post_meta( $post->ID, '_cc_featured_image_caption', $this->postmeta );
+        add_post_meta($post->ID, '_cc_featured_image_caption', $this->postmeta);
 
         // Get the output from the method
-        $test = $this->caption->caption( false );
+        $test = $this->caption->caption(false);
 
         // Array keys are all present
-        $this->assertArrayHasKey( 'caption_text', $test );
-        $this->assertArrayHasKey( 'source_text', $test );
-        $this->assertArrayHasKey( 'source_url', $test );
-        $this->assertArrayHasKey( 'new_window', $test );
+        $this->assertArrayHasKey('caption_text', $test);
+        $this->assertArrayHasKey('source_text', $test);
+        $this->assertArrayHasKey('source_url', $test);
+        $this->assertArrayHasKey('new_window', $test);
 
         // Contents of each array element
-        $this->assertEquals( $this->postmeta['caption_text'], $test['caption_text'] );
-        $this->assertEquals( $this->postmeta['source_text'], $test['source_text'] );
-        $this->assertEquals( $this->postmeta['source_url'], $test['source_url'] );
-        $this->assertTrue( $test['new_window'] );
+        $this->assertEquals($this->postmeta['caption_text'], $test['caption_text']);
+        $this->assertEquals($this->postmeta['source_text'], $test['source_text']);
+        $this->assertEquals($this->postmeta['source_url'], $test['source_url']);
+        $this->assertTrue($test['new_window']);
     }
 
     /**
      * caption() method, no data.
      */
-    public function test_caption_nodata() {
+    public function test_caption_nodata()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         global $post;
 
         // Create a new post
         $post = $this->factory->post->create_and_get();
-        setup_postdata( $post );
+        setup_postdata($post);
 
         // Get the output from the method
         $test = $this->caption->caption();
 
-        $this->assertNull( $test );
+        $this->assertNull($test);
     }
 
     /**
      * caption() method, no data, return array.
      */
-    public function test_caption_nodata_array() {
+    public function test_caption_nodata_array()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         global $post;
 
         // Create a new post
         $post = $this->factory->post->create_and_get();
-        setup_postdata( $post );
+        setup_postdata($post);
 
         // Get the output from the method
-        $test = $this->caption->caption( false );
+        $test = $this->caption->caption(false);
 
-        $this->assertNull( $test );
+        $this->assertNull($test);
     }
 
     /**
      * caption_data() method
      */
-    public function test_caption_data() {
+    public function test_caption_data()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         global $post;
 
         // Create a new post
         $post = $this->factory->post->create_and_get();
-        setup_postdata( $post );
+        setup_postdata($post);
 
         // Add the caption data to the post meta
-        add_post_meta( $post->ID, '_cc_featured_image_caption', $this->postmeta );
+        add_post_meta($post->ID, '_cc_featured_image_caption', $this->postmeta);
 
         // Get the output of the method
-        $test = $this->caption->caption_data( $post->ID );
+        $test = $this->caption->caption_data($post->ID);
 
         // Array keys are all present
-        $this->assertArrayHasKey( 'caption_text', $test );
-        $this->assertArrayHasKey( 'source_text', $test );
-        $this->assertArrayHasKey( 'source_url', $test );
-        $this->assertArrayHasKey( 'new_window', $test );
+        $this->assertArrayHasKey('caption_text', $test);
+        $this->assertArrayHasKey('source_text', $test);
+        $this->assertArrayHasKey('source_url', $test);
+        $this->assertArrayHasKey('new_window', $test);
 
         // Contents of each array element
-        $this->assertEquals( $this->postmeta['caption_text'], $test['caption_text'] );
-        $this->assertEquals( $this->postmeta['source_text'], $test['source_text'] );
-        $this->assertEquals( $this->postmeta['source_url'], $test['source_url'] );
-        $this->assertTrue( $test['new_window'] );
+        $this->assertEquals($this->postmeta['caption_text'], $test['caption_text']);
+        $this->assertEquals($this->postmeta['source_text'], $test['source_text']);
+        $this->assertEquals($this->postmeta['source_url'], $test['source_url']);
+        $this->assertTrue($test['new_window']);
     }
 
     /**
      * caption_data() method, no data.
      */
-    public function test_caption_data_nodata() {
+    public function test_caption_data_nodata()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         global $post;
 
         // Create a new post
         $post = $this->factory->post->create_and_get();
-        setup_postdata( $post );
+        setup_postdata($post);
 
         // Get the output from the method
-        $test = $this->caption->caption_data( $post->ID );
+        $test = $this->caption->caption_data($post->ID);
 
-        $this->assertNull( $test );
+        $this->assertNull($test);
     }
 
     /**
      * html() method, no shortcode attributes.
      */
-    public function test_html() {
-        // Call the html() method
-        $test = $this->caption->html( $this->postmeta );
+    public function test_html()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->string->html, $test );
+        // Call the html() method
+        $test = $this->caption->html($this->postmeta);
+
+        $this->assertEquals($this->string->html, $test);
     }
 
     /**
      * html() method, 'caption-text' shortcode attribute.
      */
-    public function test_html_caption_text() {
+    public function test_html_caption_text()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'caption-text' ) );
+        $test = $this->caption->html($this->postmeta, array( 'caption-text' ));
 
         // Expected output
         $string = '<div class="ccfic"><span class="ccfic-text">' . $this->postmeta['caption_text'] . '</span></div>';
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * html() method, 'source-text' shortcode attribute.
      */
-    public function test_html_source_text() {
+    public function test_html_source_text()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'source-text' ) );
+        $test = $this->caption->html($this->postmeta, array( 'source-text' ));
 
         // Expected output
         $string = '<div class="ccfic"> <span class="ccfic-source">' . $this->postmeta['source_text'] . '</span></div>';
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * html() method, 'caption-text' and 'source-text' shortcode attributes.
      */
-    public function test_html_caption_text_source_text() {
+    public function test_html_caption_text_source_text()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'caption-text', 'source-text' ) );
+        $test = $this->caption->html($this->postmeta, array( 'caption-text', 'source-text' ));
 
         // Expected output
         $string = '<div class="ccfic"><span class="ccfic-text">' . $this->postmeta['caption_text'] . '</span> <span class="ccfic-source">' . $this->postmeta['source_text'] . '</span></div>';
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * html() method, just 'source-link' shortcode attribute.
      */
-    public function test_html_source_link() {
+    public function test_html_source_link()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'source-link' ) );
+        $test = $this->caption->html($this->postmeta, array( 'source-link' ));
 
         // Expected output
         $string = '<div class="ccfic"> <span class="ccfic-source"><a href="' . $this->postmeta['source_url'] . '" target="_blank">' . $this->postmeta['source_text'] . '</a></span></div>';
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * html() method, all shortcode attributes.
      */
-    public function test_html_all_shortcode_atts() {
-        // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'caption-text', 'source-text', 'source-link' ) );
+    public function test_html_all_shortcode_atts()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->string->html, $test );
+        // Call the html() method
+        $test = $this->caption->html($this->postmeta, array( 'caption-text', 'source-text', 'source-link' ));
+
+        $this->assertEquals($this->string->html, $test);
     }
 
     /**
      * html() method, gibberish shortcode attribute.
      */
-    public function test_html_gibberish_shortcode_att() {
+    public function test_html_gibberish_shortcode_att()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'faw3aardvwasfeasdfasdf' ) );
+        $test = $this->caption->html($this->postmeta, array( 'faw3aardvwasfeasdfasdf' ));
 
         // Expected output
         $string = '<div class="ccfic"></div>';
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * html() method, gibberish shortcode attribute combined with real attribute.
      */
-    public function test_html_gibberish_combo_shortcode_atts() {
+    public function test_html_gibberish_combo_shortcode_atts()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the html() method
-        $test = $this->caption->html( $this->postmeta, array( 'faw3aardvwasfeasdfasdf', 'caption-text' ) );
+        $test = $this->caption->html($this->postmeta, array( 'faw3aardvwasfeasdfasdf', 'caption-text' ));
 
         // Expected output
         $string = '<div class="ccfic"><span class="ccfic-text">' . $this->postmeta['caption_text'] . '</span></div>';
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * plaintext() method, no shortcode attributes.
      */
-    public function test_plaintext() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta );
+    public function test_plaintext()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->string->plaintext, $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta);
+
+        $this->assertEquals($this->string->plaintext, $test);
     }
 
     /**
      * plaintext() method, 'caption-text' shortcode attribute.
      */
-    public function test_plaintext_caption_text() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( 'caption-text' ) );
+    public function test_plaintext_caption_text()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->postmeta['caption_text'], $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta, array( 'caption-text' ));
+
+        $this->assertEquals($this->postmeta['caption_text'], $test);
     }
 
     /**
      * plaintext() method, 'source-text' shortcode attribute.
      */
-    public function test_plaintext_source_text() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( 'source-text' ) );
+    public function test_plaintext_source_text()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->postmeta['source_text'], $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta, array( 'source-text' ));
+
+        $this->assertEquals($this->postmeta['source_text'], $test);
     }
 
     /**
      * plaintext() method, 'caption-text' and 'source-text' shortcode attributes.
      */
-    public function test_plaintext_caption_text_source_text() {
+    public function test_plaintext_caption_text_source_text()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( 'caption-text', 'source-text' ) );
+        $test = $this->caption->plaintext($this->postmeta, array( 'caption-text', 'source-text' ));
 
         // Expected output
         $string = $this->postmeta['caption_text'] . ' ' . $this->postmeta['source_text'];
 
-        $this->assertEquals( $string, $test );
+        $this->assertEquals($string, $test);
     }
 
     /**
      * plaintext() method, just 'source-url' shortcode attribute.
      */
-    public function test_plaintext_source_url() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( 'source-url' ) );
+    public function test_plaintext_source_url()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->postmeta['source_url'], $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta, array( 'source-url' ));
+
+        $this->assertEquals($this->postmeta['source_url'], $test);
     }
 
     /**
      * plaintext() method, all shortcode attributes.
      */
-    public function test_plaintext_all_shortcode_atts() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( 'caption-text', 'source-text', 'source-url' ) );
+    public function test_plaintext_all_shortcode_atts()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->postmeta['source_url'], $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta, array( 'caption-text', 'source-text', 'source-url' ));
+
+        $this->assertEquals($this->postmeta['source_url'], $test);
     }
 
     /**
      * plaintext() method, gibberish shortcode attribute.
      */
-    public function test_plaintext_gibberish_shortcode_att() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( '65rgtfythdrgs' ) );
+    public function test_plaintext_gibberish_shortcode_att()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEmpty( $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta, array( '65rgtfythdrgs' ));
+
+        $this->assertEmpty($test);
     }
 
     /**
      * plaintext() method, gibberish shortcode attribute combined with real attribute.
      */
-    public function test_plaintext_gibberish_combo_shortcode_atts() {
-        // Call the plaintext() method
-        $test = $this->caption->plaintext( $this->postmeta, array( '65rgtfythdrgs', 'caption-text' ) );
+    public function test_plaintext_gibberish_combo_shortcode_atts()
+    {
+        // Set up test data
+        $this->set_test_data();
 
-        $this->assertEquals( $this->postmeta['caption_text'], $test );
+        // Call the plaintext() method
+        $test = $this->caption->plaintext($this->postmeta, array( '65rgtfythdrgs', 'caption-text' ));
+
+        $this->assertEquals($this->postmeta['caption_text'], $test);
     }
 
     /**
      * auto_append() method
      */
-    public function test_auto_append() {
+    public function test_auto_append()
+    {
+        // Set up test data
+        $this->set_test_data();
+
         $test = $this->caption->auto_append();
 
-        $this->assertTrue( $test );
+        $this->assertTrue($test);
     }
 }
